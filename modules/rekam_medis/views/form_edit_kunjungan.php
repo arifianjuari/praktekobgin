2bfb2764263ce34feaea0ae3ecb0056a430799f2
arifianjuari <arifianjuari@gmail.com>
2025-07-13 11:50:05 +0700

    Update database credentials in form_edit_kunjungan.php

--- modules/rekam_medis/views/form_edit_kunjungan.php
+++ modules/rekam_medis/views/form_edit_kunjungan.php
@@ -188,8 +188,8 @@ if (!isset($kunjungan) || !$kunjungan) {
                             // Fallback jika env kosong
                             // if (empty($db2_host)) $db2_host = '127.0.0.1';
                             if (empty($db2_host)) $db2_host = 'localhost';
-                            if (empty($db2_username)) $db2_username = 'root';
-                            if (empty($db2_password)) $db2_password = 'root';
+                            if (empty($db2_username)) $db2_username = 'arifianjuari';
+                            if (empty($db2_password)) $db2_password = 'Juari@2591';
                             if (empty($db2_database)) $db2_database = 'praktekobgin_db';
                             if (empty($db2_port)) $db2_port = '8889';
                             // $conn = new mysqli($db2_host, $db2_username, $db2_password, $db2_database, $db2_port);
